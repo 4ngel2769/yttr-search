d0abbf337e1fdd5a2ae132c44ac1e8c1fa15b7bf
4ngel2769 <angeldevz0@proton.me>
2026-01-02 17:10:22 +0200

    Create global.d.ts

---
+++ web/src/global.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="next" />
+
+// CSS Module declarations
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+// Global CSS declarations
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
